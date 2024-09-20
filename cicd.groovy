c6e54deb23c6de8c331a0aca2665766ad3ab84d4
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:20:23 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -7,9 +7,9 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: "https://github.com/ZOSOpenTools/ninjaport.git"]]])
+                        userRemoteConfigs: [[url: "https://github.com/zopencommunity/ninjaport.git"]]])
         }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/ninjaport.git'), string(name: 'PORT_DESCRIPTION', value: 'Ninja is a small build system with a focus on speed.' )]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/ninjaport.git'), string(name: 'PORT_DESCRIPTION', value: 'Ninja is a small build system with a focus on speed.' )]
   }
 }
